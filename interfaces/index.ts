490059bbc981da43dae3a7d5fd93c2742ef122e6
Enoch Okine Mensah <okaycode100@gmail.com>
2025-09-20 22:39:55 +0000

    Responsive Listing Page

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,39 @@
-export function InterfaceExample() {
-  return "This is an example interface function.";
+export interface PropertyProps {
+  name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers;
+  image: string;
+  discount: string;
+}
+export interface Address {
+  state: string;
+  city: string;
+  country: string;
+}
+export interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PillProps {
+  label: string;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+export interface PillProps {
+  label: string;  
+  active?: boolean;
+  onClick?: () => void;
+}
+
+export interface PropertyCardsProps {
+  name: string;
+  image: string;
+  rating: number;
+  price: string;
 }
